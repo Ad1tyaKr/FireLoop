a3f746a1ff06fc565fec69ecd348f6a19f7f867c
Aditya Kumar <158069221+Ad1tyaKr@users.noreply.github.com>
2025-06-27 15:13:21 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '/FireLoop', 
+
   eslint: {
     ignoreDuringBuilds: true,
   },
